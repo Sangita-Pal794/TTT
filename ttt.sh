0863b11fa8a3dd0cb92aedd2aeefb1dc9d356534
Sangita <pal07sangita@gmail.com>
2020-09-19 19:01:05 +0530

    Added UC3:Display board

--- ttt.sh
+++ ttt.sh
@@ -1,10 +1,31 @@
-#!/bin/bash -x
+#!/bin/bash
+
+COL_SIZE=3
+CENTER=4
+BOARD_SIZE=9
+WIN_SET=(012 345 678 036 147 258 048 246)
+
+#variables
 declare -a gameBoard
 gameBoard=(- - - - - - - - -)
-#toss=$(($RANDOM%2))
-if [ $(($RANDOM%2)) -eq 0 ]
-  then
-    echo X\'s TURN
- else
-	echo O\'s TURN
- fi
+toss=$(($RANDOM%2))
+winStatus=0
+insertStatus=0
+colCount=0
+
+displayBoard(){
+	for((cell=0;cell<$BOARD_SIZE;cell++))
+	do
+		if [[ $colCount -eq $COL_SIZE ]]
+		then
+			echo ""
+			colCount=0
+		fi
+		echo -e "${gameBoard[$cell]}\t \c"
+		((colCount++))
+	done
+	echo ""
+}
+
+displayBoard
+
